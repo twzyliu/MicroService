e480d6fae02e22025dafc1ffbc527fc148aa8896
Your Name <you@example.com>
2018-04-16 00:07:53 +0800

    fix createUnloading

--- orderService/src/main/java/com/thoughtworks/ketsu/web/OrderApi.java
+++ orderService/src/main/java/com/thoughtworks/ketsu/web/OrderApi.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.ketsu.web;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.thoughtworks.ketsu.infrastructure.repositories.OrderRepository;
-import org.json.JSONObject;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -16,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.alibaba.fastjson.JSON.toJSONString;
 import static com.thoughtworks.ketsu.util.HttpClient.doGet;
 import static com.thoughtworks.ketsu.util.HttpClient.doPost;
 import static java.lang.Integer.parseInt;
@@ -48,16 +47,17 @@ public class OrderApi {
     private void createUnloading(List<Map> order_items) throws Exception {
         String host = getenv().getOrDefault("INVENTORY_SERVICE_HOST", "127.0.0.1");
         String port = getenv().getOrDefault("INVENTORY_SERVICE_PORT", "23333");
-        List<Map> uploading = order_items.stream().map(order_item -> new HashMap() {{
+        List<Map> uploadings = order_items.stream().map(order_item -> new HashMap() {{
             put("product_id", order_item.get("product_id"));
             put("quantity", order_item.get("quantity"));
             put("pay", "0");
         }}).collect(toList());
+        String unloadings_str = toJSONString(uploadings);
         Map uploadingMap = new HashMap() {{
-            put("upploading", uploading);
+            put("upploading", unloadings_str);
         }};
-        String uploading_str = new Gson().toJson(uploadingMap);
-        doPost("http://" + host + ":" + port + "/unloadings", uploading_str);
+        String uploading_json = new Gson().toJson(uploadingMap, Map.class);
+        doPost("http://" + host + ":" + port + "/unloadings", uploading_json);
     }
 
     private String getPrice(Map order_item) {
