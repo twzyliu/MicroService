e14f6e37e762919112cf8ba57f21a7c1611c0410
Your Name <you@example.com>
2018-04-15 15:22:39 +0800

    should_call_price_service_when_create_orderitem

--- orderService/src/main/java/com/thoughtworks/ketsu/web/OrderApi.java
+++ orderService/src/main/java/com/thoughtworks/ketsu/web/OrderApi.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.ketsu.web;
 
+import com.google.gson.Gson;
 import com.thoughtworks.ketsu.infrastructure.repositories.OrderRepository;
 
 import javax.inject.Inject;
@@ -8,9 +9,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 
-import static javax.ws.rs.core.Response.*;
+import static com.thoughtworks.ketsu.util.HttpClient.doGet;
+import static java.lang.Integer.*;
+import static javax.ws.rs.core.Response.created;
+import static javax.ws.rs.core.Response.status;
 
 @Path("/orders")
 public class OrderApi {
@@ -19,8 +26,23 @@ public class OrderApi {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(Map<String, Object> info) {
+    public Response create(Map<String, Object> info) throws URISyntaxException {
+        List<Map> order_items = (List<Map>) info.get("order_items");
+        String amount = order_items.stream().
+                map(order_item -> parseInt(getPrice(order_item)) * parseInt(order_item.get("quantity") + "")).
+                reduce(0, (sum, element) -> sum + element) + "";
+
+        info.put("amount", amount);
         orderRepository.save(info);
-        return status(201).build();
+        return info.containsKey("id") ?
+                created(new URI("/orders/" + info.get("id"))).build() :
+                status(400).build();
+    }
+
+    private String getPrice(Map order_item) {
+        String price_service_host_host = System.getenv().getOrDefault("PRICE_SERVICE_HOST", "127.0.0.1");
+        String price_service_port = System.getenv().getOrDefault("PRICE_SERVICE_PORT", "21000");
+        String priceMap = doGet("http://" + price_service_host_host + ":" + price_service_port + "/prices?product_id=" + order_item.get("product_id"));
+        return new Gson().fromJson(priceMap, Map.class).get("price") + "";
     }
 }
--- orderService/src/test/java/com/thoughtworks/ketsu/web/OrderApiTest.java
+++ orderService/src/test/java/com/thoughtworks/ketsu/web/OrderApiTest.java
@@ -1,9 +1,15 @@
 package com.thoughtworks.ketsu.web;
 
+import com.google.gson.Gson;
 import com.thoughtworks.ketsu.support.ApiSupport;
 import com.thoughtworks.ketsu.support.ApiTestRunner;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.Parameter;
 
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -12,26 +18,61 @@ import java.util.Map;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 @RunWith(ApiTestRunner.class)
-public class OrderApiTest extends ApiSupport{
+public class OrderApiTest extends ApiSupport {
+
+    private MockServerClient mockClient;
+
+    @Rule
+    public MockServerRule server = new MockServerRule(this, 21000);
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        mockClient = new MockServerClient("127.0.0.1", 21000);
+    }
+
     @Test
     public void should_return_201_when_post_order_success() throws Exception {
+        String product_id = "001";
+        String price = "100";
+        String quantity = "10";
+        Map price_map = new HashMap() {{
+            put("id", "1234567890");
+            put("product_id", product_id);
+            put("price", price);
+        }};
         Map order_item_map = new HashMap() {{
-            put("product_id", "001");
-            put("quantity", "10");
+            put("product_id", product_id);
+            put("quantity", quantity);
         }};
         Map order_map = new HashMap() {{
             put("user_id", "007");
             put("name", "Hades");
             put("phone", "999");
             put("address", "home");
-            put("amount", 100);
             put("order_items", asList(order_item_map));
         }};
+
+        String expected = new Gson().toJson(price_map, Map.class);
+        mockClient.when(
+                request()
+                        .withPath("/prices")
+                        .withMethod("GET")
+                        .withQueryStringParameter(new Parameter("product_id", product_id))
+        ).respond(
+                response()
+                        .withStatusCode(200)
+                        .withBody(expected)
+        );
+
         Response post = post("/orders", order_map);
 
+        Map order = post.readEntity(Map.class);
         assertThat(post.getStatus(), is(201));
     }
-
 }
