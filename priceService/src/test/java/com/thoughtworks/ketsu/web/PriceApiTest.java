c7ef4d951afb77885366bb1eecce6026a99bf596
Your Name <you@example.com>
2018-04-16 19:38:36 +0800

    fix get price test

--- priceService/src/test/java/com/thoughtworks/ketsu/web/PriceApiTest.java
+++ priceService/src/test/java/com/thoughtworks/ketsu/web/PriceApiTest.java
@@ -13,27 +13,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(ApiTestRunner.class)
-public class PriceApiTest extends ApiSupport{
+public class PriceApiTest extends ApiSupport {
     @Test
     public void should_return_201_when_post_price_success() throws Exception {
-        Response post = post("/prices", new HashMap(){{
-            put("product_id","1");
-            put("price","999");
+        Response post = post("/prices", new HashMap() {{
+            put("product_id", "1");
+            put("price", "999");
         }});
         assertThat(post.getStatus(), is(201));
     }
 
     @Test
     public void should_return_400_when_post_price_success() throws Exception {
-        Response post = post("/prices", new HashMap(){{
-            put("price","999");
+        Response post = post("/prices", new HashMap() {{
+            put("price", "999");
         }});
         assertThat(post.getStatus(), is(400));
     }
 
-//    @Test
+    @Test
     public void should_return_200_when_get_price() throws Exception {
-        Response get = get("/prices?product_id=100");
+        Response get = session(target("/prices").queryParam("product_id", "100").request()).get();
         assertThat(get.getStatus(), is(200));
     }
 }
