a2bf02291fc5e0921f6bda6b13d3912d9a1877c7
Your Name <you@example.com>
2018-04-18 14:26:25 +0800

    should_call_order_service_when_create_returnOrder

--- refundService/src/main/java/com/thoughtworks/ketsu/web/ReturnOrdersApi.java
+++ refundService/src/main/java/com/thoughtworks/ketsu/web/ReturnOrdersApi.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.ketsu.web;
 
+import com.google.gson.Gson;
 import com.thoughtworks.ketsu.infrastructure.repositories.ReturnOrderRepository;
 
 import javax.inject.Inject;
@@ -12,7 +13,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
 
-import static javax.ws.rs.core.Response.*;
+import static com.thoughtworks.ketsu.util.HttpClient.doGet;
+import static java.lang.System.getenv;
+import static javax.ws.rs.core.Response.created;
+import static javax.ws.rs.core.Response.status;
 
 @Path("/return_orders")
 public class ReturnOrdersApi {
@@ -22,9 +26,19 @@ public class ReturnOrdersApi {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response create(Map<String, Object> info) throws URISyntaxException {
+        Map order_map = getOrderMap(info.get("order_id") + "");
+        
+        info.put("amount", "0");
         returnOrderRepository.save(info);
         return info.containsKey("id") ?
                 created(new URI("/return_orders/" + info.get("id"))).build() :
                 status(400).build();
+    }
+
+    private Map getOrderMap(String oid) {
+        String host = getenv().getOrDefault("ORDER_SERVICE_HOST", "127.0.0.1");
+        String port = getenv().getOrDefault("ORDER_SERVICE_PORT", "23333");
+        String order_json = doGet("http://" + host + ":" + port + "/orders/" + oid);
+        return new Gson().fromJson(order_json, Map.class);
     }
 }
--- refundService/src/test/java/com/thoughtworks/ketsu/web/ReturnOrderApiTest.java
+++ refundService/src/test/java/com/thoughtworks/ketsu/web/ReturnOrderApiTest.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.ketsu.web;
 
+import com.google.gson.Gson;
 import com.thoughtworks.ketsu.support.ApiSupport;
 import com.thoughtworks.ketsu.support.ApiTestRunner;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.junit.MockServerRule;
 
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -12,20 +17,53 @@ import java.util.Map;
 import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 @RunWith(ApiTestRunner.class)
-public class ReturnOrderApiTest extends ApiSupport{
+public class ReturnOrderApiTest extends ApiSupport {
+    private MockServerClient mockClient;
+
+    @Rule
+    public MockServerRule server = new MockServerRule(this, 23333);
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        mockClient = new MockServerClient("127.0.0.1", 23333);
+    }
+
     @Test
     public void should_return_201_when_post_returnOrder_success() throws Exception {
+        String order_id = "1";
+        String quantity = "9";
+        String product_id = "1";
+
+        Map price_map = new HashMap() {{
+            put("order_items", "[{\"product_id\":\"" + product_id + "\",\"quantity\":\"" + quantity + "\"}]");
+            put("amount", "1");
+        }};
+
+        String get_body = new Gson().toJson(price_map, Map.class);
+        mockClient.when(
+                request()
+                        .withPath("/orders/" + order_id)
+                        .withMethod("GET")
+        ).respond(
+                response()
+                        .withStatusCode(200)
+                        .withBody(get_body)
+        );
+
         Map return_order_item = new HashMap() {{
             put("return_order_id", "1");
-            put("product_id", "1");
-            put("quantity", "9");
+            put("product_id", product_id);
+            put("quantity", quantity);
         }};
-        Response post = post("/return_orders", new HashMap(){{
-            put("order_id","1");
-            put("amount","99");
-            put("return_order_items",asList(return_order_item));
+        Response post = post("/return_orders", new HashMap() {{
+            put("order_id", order_id);
+            put("return_order_items", asList(return_order_item));
         }});
         assertThat(post.getStatus(), is(201));
     }
